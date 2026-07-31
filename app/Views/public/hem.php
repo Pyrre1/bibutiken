e3a36df5def0e20b73078047145789d915cde115
Emanuel U <eu222ei@student.lnu.se>
2026-07-31 20:12:57 +0200

    Fix: Minor wording and cleanup, internal links for ease of use

--- app/Views/public/hem.php
+++ app/Views/public/hem.php
@@ -52,7 +52,7 @@
 <!-- OM BUTIKEN -->
 <section class="info-section" id="om-butiken">
     <h2>Om butiken</h2>
-    <p>Vi som bor på Rosenborg och driver bibutiken heter Hanne och Andreas. Hanne har varit biodlare sedan 2008. </p>
+    <p>Vi som bor på Rosenborg och driver bibutiken heter Hanne och Andreas. Vi började med bin 2008. </p>
     <p>Strängnäs Biredskap öppnade 2012. I butiken säljer vi biredskap från Biredskapsfabriken i Töreboda samt böcker 
         och andra biprylar från olika leverantörer. Butiken har inga fasta öppettider utan vi har öppet när vi är hemma.</p>
 </section>
@@ -61,9 +61,11 @@
 <section class="info-section" id="vad-kan-jag-kopa">
     <h2>Vad kan jag köpa här?</h2>
     <ul class="content-list">
-        <h3>Är du inte biodlare, utan är intresserad av bigårdens produkter?</h3>
-        <li> För köp av honung, pollen eller andra produkter från biodlingen, se <a href="/biprodukter">tillgängliga produkter</a>.</li>
-        <h3>Är du biodlare och vill köpa biredskap?</h3>
+        <h3>Är du inte biodlare, men är intresserad av biprodukter?</h3>
+        <li> För köp av honung (pressad, slungad eller kankse samlad från framträdande blommor), se <a href="/biprodukter">tillgängliga produkter</a>.</li>
+        <li> Förutom honung pruducerar bin även pollen, vax och propolis som vi också tar hand om och säljer eller förädlar till schampo, läppcerat och salva. 
+            Det finns även vax, bibröd och böcker m.m. se <a href="/biprodukter#biprodukter">relaterade produkter</a>.</li>
+        <h3 class="biodlare-intresse">Är du biodlare och vill köpa biredskap?</h3>
         <li>Under säsong går det att köpa invintringsfoder. För att säkerställa att vi beställer tillräckligt mycket foder går det att fylla i en förbeställning på <a href="/vinterfoder">vinterfoder</a>. När fodret kommit till butiken får du mejl om att hämta och fakturan kommer efter att produkterna hämtats.</li>
         <li>Vi har nästan alltid honungsburkar i de vanligaste storlekarna hemma. Vax, kupor och ramar till LN och HLS är också sådant vi alltid försöker ha hemma. Om du vill ha isolerade träkupor eller ramar/vax i andra storlekar kan vi ta hem efter önskemål. <br>Du kan se sortimentet på Biredskapsfabrikens hemsida <a href="https://www.biredskapsfabriken.se/" target="_blank" rel="noopener noreferrer">https://www.biredskapsfabriken.se/</a></li>
         <li>I bibutiken kan du också hyra en tvåramars slunga (som tar 4 HLS ramar) samt en ångvaxsmältare. Här finns en refraktometer om du vill ta med dig honung för att kolla vattnehalten i honungen.</li>
@@ -88,9 +90,11 @@
     <div class="faq-item">
         <h3 id="faq-vax">Kan man lämna in vax i butiken?</h3>
         <p>Ja, utsmält vax som är grovrensat och där pollen och skräp är bortskuret kan man lämna året om. Du kan välja mellan
-            att byta till motsvarande mängd mellanväggar (du betalar för valsningen) eller sälja vaxet (till aktuellt dagspris). 
-            Täckvax och vax i ram kan man lämna in 1 oktober-1 mars, <strong>utanför dessa tider  får du inte flytta ramar som inte är besiktigade av bitillsyningsman.</strong>. Då betalar du för rengöring av ramar, rensning och valsning 
-            av vax och en mindre peng för transporten. 
+            att byta till motsvarande mängd mellanväggar (du betalar för valsningen) eller sälja vaxet (till aktuellt dagspris). <br>
+            Täckvax och vax i ram kan man lämna in från <strong>första oktober till och med första mars (1/10-1/3)</strong>. Då betalar du för rengöring av ramar, rensning och valsning 
+            av vax och en mindre peng för transporten. <br>
+            Du kommer inte få tillbaka just ditt vax, men om du märker vaxet med ”Särbehandlas” samt
+            lämnar ett intyg så hanteras ditt vax tillsammans med annat vax som också har intyg.
         </p>
     </div>
 
